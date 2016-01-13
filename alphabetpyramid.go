80b000f5d82ae7211e705363d33c70e283956969
Charles Korn <me@charleskorn.com>
2016-01-13 20:35:19 +1100

    Added command line argument validation.

--- alphabetpyramid.go
+++ alphabetpyramid.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"os"
+	"regexp"
+)
 
+type OutputWriter interface {
+	WriteString(s string) (n int, err error)
+}
+
+func main() {
+	run(os.Args[1:], os.Stdout)
+}
+
+func run(args []string, output OutputWriter) {
+	if !validateArguments(args) {
+		output.WriteString("INVALID INPUT\n")
+		return
+	}
+}
+
+func validateArguments(args []string) bool {
+	if len(args) != 1 {
+		return false
+	}
+
+	pattern := regexp.MustCompile(`^[A-Za-z]$`)
+
+	return pattern.MatchString(args[0])
 }
--- alphabetpyramid_test.go
+++ alphabetpyramid_test.go
@@ -1,4 +1,4 @@
-package main_test
+package main
 
 import (
 	. "github.com/onsi/ginkgo"
@@ -11,8 +11,76 @@ func TestSuite(t *testing.T) {
 	RunSpecs(t, "Alphabet Pyramid Suite")
 }
 
-var _ = Describe("The application", func() {
-	It("does something", func() {
+type StringOutputWriter struct {
+	Output string
+}
 
+func (w *StringOutputWriter) WriteString(s string) (n int, err error) {
+	w.Output += s
+
+	return len(s), nil
+}
+
+var _ = Describe("run", func() {
+	runApplication := func(args []string) string {
+		outputWriter := &StringOutputWriter{}
+		run(args, outputWriter)
+		return outputWriter.Output
+	}
+
+	Context("when no arguments are provided", func() {
+		It("prints 'INVALID INPUT'", func() {
+			output := runApplication([]string{})
+
+			Expect(output).To(Equal("INVALID INPUT\n"))
+		})
+	})
+
+	Context("when an invalid argument is provided", func() {
+		It("prints 'INVALID INPUT'", func() {
+			output := runApplication([]string{"ABCD"})
+
+			Expect(output).To(Equal("INVALID INPUT\n"))
+		})
+	})
+
+	Context("when a valid argument is provided", func() {
+		It("prints nothing", func() {
+			output := runApplication([]string{"C"})
+
+			Expect(output).To(Equal(""))
+		})
+	})
+})
+
+var _ = Describe("validateArguments", func() {
+	Context("when no arguments are provided", func() {
+		It("returns false", func() {
+			Expect(validateArguments([]string{})).To(BeFalse())
+		})
+	})
+
+	Context("when a single letter is provided", func() {
+		It("returns true", func() {
+			Expect(validateArguments([]string{"A"})).To(BeTrue())
+		})
+	})
+
+	Context("when multiple arguments are provided", func() {
+		It("returns false", func() {
+			Expect(validateArguments([]string{"A", "B"})).To(BeFalse())
+		})
+	})
+
+	Context("when multiple letters are provided", func() {
+		It("returns false", func() {
+			Expect(validateArguments([]string{"AB"})).To(BeFalse())
+		})
+	})
+
+	Context("when a single non-letter character is provided", func() {
+		It("returns false", func() {
+			Expect(validateArguments([]string{"1"})).To(BeFalse())
+		})
 	})
 })
